697dc78387508445fc668376e8f766f81fe45ce8
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-07-14 17:12:52 +0300

    слайдер с портфолио без мобилки

--- layout/js/moduls/portfolio.js
+++ layout/js/moduls/portfolio.js
@@ -1,44 +1,46 @@
 "use strict";
 
-const portfolio = ()=>{
+const portfolio = () => {
   const wrap = document.querySelector('.portfolio-slider-wrap');
-  const arrowLeft = wrap.querySelector('.slider-arrow_left-portfolio');
-  const arrowRight = wrap.querySelector('.slider-arrow_right-portfolio');
   const slider = wrap.querySelector('.portfolio-slider');
   const row = slider.querySelector('.row-slide');
-  const cols = wrap.querySelectorAll('.portfolio-slider__slide');
-  const widthWndow = slider.clientWidth;
-  const widthElem = cols[0].clientWidth;
 
-  const maxCounter = cols.length - widthWndow/widthElem;
-  let counter = 0;
+  const main = () => {
+    const arrowLeft = wrap.querySelector('.slider-arrow_left-portfolio');
+    const arrowRight = wrap.querySelector('.slider-arrow_right-portfolio');
+    const cols = wrap.querySelectorAll('.portfolio-slider__slide');
+    const widthWndow = slider.clientWidth;
+    const widthElem = cols[0].clientWidth;
 
-  const check = (i, max=maxCounter)=>{
-    let k = i;
-    arrowLeft.style.visibility='visible';
-    arrowRight.style.visibility='visible';
-    if (k<=0) {
-      arrowLeft.style.visibility='hidden';
-      k=i++;
-    }
-    if (k>=max) {
-      arrowRight.style.visibility='hidden';
-      k=i--;
-    }
-    return k;
-  };
+    const maxCounter = cols.length - widthWndow / widthElem;
+    let counter = 0;
 
-  check(counter);
+    const check = (i, max = maxCounter) => {
+      let k = i;
+      arrowLeft.style.visibility = 'visible';
+      arrowRight.style.visibility = 'visible';
+      if (k <= 0) {
+        arrowLeft.style.visibility = 'hidden';
+        k = i++;
+      }
+      if (k >= max) {
+        arrowRight.style.visibility = 'hidden';
+        k = i--;
+      }
+      return k;
+    };
 
-  arrowRight.addEventListener('click',()=>{
-    counter = check(++counter);
-    row.style.transform= `translateX(-${widthElem*counter}px)`;
-  });
+    check(counter);
 
-  arrowLeft.addEventListener('click',()=>{
-    counter = check(--counter);
-    row.style.transform= `translateX(-${widthElem*counter}px)`;
-  });
+    arrowRight.addEventListener('click', () => {
+      counter = check(++counter);
+      row.style.transform = `translateX(-${widthElem*counter}px)`;
+    });
+
+    arrowLeft.addEventListener('click', () => {
+      counter = check(--counter);
+      row.style.transform = `translateX(-${widthElem*counter}px)`;
+    });
 
     const style = document.createElement('style');
     style.textContent = `
@@ -47,7 +49,119 @@ const portfolio = ()=>{
        transition: all 1s ease 0s;
     }
     `;
-    document.querySelector('head').insertAdjacentElement('beforeend',style);
+    document.querySelector('head').insertAdjacentElement('beforeend', style);
+  };
+  main();
+
+  const popup = () => {
+    //модалочка popup-dialog-portfolio
+    const popupPortfolio = document.querySelector('.popup-portfolio');
+    const popupDialogPortfolio = document.querySelector('.popup-dialog-portfolio');
+    const close = popupPortfolio.querySelector('.close ');
+    const popupTextMas = document.querySelectorAll('.popup-portfolio-text');
+    const slidesMas = row.querySelectorAll('.portfolio-slider__slide-frame');
+    const popupPortfolioSlider = popupPortfolio.querySelector('.popup-portfolio-slider');
+    const imgMas = popupPortfolioSlider.querySelectorAll('.popup-portfolio-slider__slide');
+    const arr = [];
+    const sliderCounterContentCurrent = popupPortfolio.querySelector('.slider-counter-content__current');
+    const sliderCounterContentTotal = popupPortfolio.querySelector('.slider-counter-content__total');
+
+
+
+    for (let i = 0; i < slidesMas.length; i++) {
+      arr[i] = {
+        slide: slidesMas[i],
+        text: popupTextMas[i],
+        img: imgMas[i],
+      };
+    }
+
+    sliderCounterContentTotal.textContent = arr.length;
+
+    const outsideClick = (e) => {
+      const target = e.target;
+
+      if (target.closest('.popup-dialog-portfolio') && target !== close) {
+        return;
+      }
+
+      document.removeEventListener('click', outsideClick);
+      close.click();
+    };
+
+    let data;
+    let currentCounter;
+
+    const removeText = () => {
+      if (popupDialogPortfolio.clientWidth <= 768) {
+        data.text.style.display = 'none';
+
+      } else {
+        data.text.style.display = 'block';
+      }
+    };
+
+    slidesMas.forEach(slide => slide.addEventListener('click', () => {
+
+      popupPortfolio.style.visibility = 'visible';
+      data = arr.filter(item => item.slide === slide)[0];
+
+      popupPortfolioSlider.prepend(data.img);
+      removeText();
+
+      setTimeout(() => {
+        document.addEventListener('click', outsideClick);
+      }, 100);
+
+      currentCounter = arr.findIndex(item=>item===data);
+      sliderCounterContentCurrent.textContent = currentCounter+1;
+
+    }));
+
+    close.addEventListener('click', () => {
+      popupPortfolio.style.visibility = 'hidden';
+      data.text.style.display = 'none';
+    });
+
+    window.addEventListener('resize', () => {
+      removeText();
+    });
+
+
+
+    const arrowLeft = popupPortfolio.querySelector('.popup-arrow_left');
+    const arrowRight = popupPortfolio.querySelector('.popup-arrow_right');
+
+
+    const check = (i,max=arr.length-1)=>{
+      let k = i;
+      if (k<0) {
+        k=max;
+      }
+      if (k>max) {
+        k=0;
+      }
+      return k;
+    };
+
+
+    const switchFoto =(value)=>{
+      popupPortfolioSlider.prepend(arr[value].img);
+      sliderCounterContentCurrent.textContent = value+1;
+    };
+
+    arrowRight.addEventListener('click', () => {
+      currentCounter = check(++currentCounter);
+      switchFoto(currentCounter);
+    });
+
+    arrowLeft.addEventListener('click', () => {
+      currentCounter = check(--currentCounter);
+      switchFoto(currentCounter);
+    });
+  };
+  popup();
+
 };
 
 export default portfolio;
